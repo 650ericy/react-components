d67963270dc99f3bbac87bc28a5257b93ac317dc
Eric Yap <ericy2296@gmail.com>
2024-02-05 11:39:37 -0800

    completed BMRs

--- app.js
+++ app.js
@@ -1,1 +1,25 @@
 // TODO
+const GroceryListItem = ({ prop }) => (
+  <li className="li-hover">{prop}</li>
+
+)
+
+//<li>{prop}</li>;
+
+//takes in array of prop, hence props
+//iterate with map and then render prop
+const GroceryList = ({ props }) => (
+  <ul>
+    {props.map((prop, index) => (
+      <GroceryListItem key={index} prop={prop} />
+    ))}
+  </ul>
+);
+
+let groceryItems = ['Cucumber', 'Kale'];
+// array with items, +
+
+//render GroceryList inside Dom
+ReactDOM.render(<GroceryList props={groceryItems} />, document.getElementById('app'));
+
+console.log('working');
